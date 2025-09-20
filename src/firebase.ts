46293a424e99e56b29bdb724732f72b6fc4fdaaa
Martin Sagat <martin.sagat@sap.com>
2025-09-20 23:24:34 +0200

    Step 4 — Setup Firebase (backend)

---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQQyBlwyw1WLF_Xkm60r89LebST8LjEHs",
+  authDomain: "lucenske-tulene-ff2f7.firebaseapp.com",
+  projectId: "lucenske-tulene-ff2f7",
+  storageBucket: "lucenske-tulene-ff2f7.firebasestorage.app",
+  messagingSenderId: "134589697796",
+  appId: "1:134589697796:web:80363952dc8e2108bdca51"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
