9a69a3ef3a32c144fe9607a512d8ea70aafbc6a2
Martin Sagat <martin.sagat@sap.com>
2025-09-22 21:53:08 +0200

    update gitignore, firebase ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAQQyBlwyw1WLF_Xkm60r89LebST8LjEHs",
-  authDomain: "lucenske-tulene-ff2f7.firebaseapp.com",
-  projectId: "lucenske-tulene-ff2f7",
-  storageBucket: "lucenske-tulene-ff2f7.firebasestorage.app",
-  messagingSenderId: "134589697796",
-  appId: "1:134589697796:web:80363952dc8e2108bdca51"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
